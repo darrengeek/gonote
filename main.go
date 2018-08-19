67d9e13085cdf292b4343c5a2168c01409cbc316
xmge <1206964123@qq.com>
2018-08-19 10:53:34 +0800

    update谈谈ok那些事儿

--- main.go
+++ main.go
@@ -2,10 +2,29 @@ package main
 
 import "fmt"
 
+// ok断言之通道有值
 func main() {
 
-	var v interface{} = 1.0
-	v1, ok := v.(float32)
-	fmt.Println(v1)
-	fmt.Println(ok)
+	c := make(chan bool,2)
+
+	<- c
+	c <- true
+	c <- true
+
+	// 通道未关闭
+	b1,ok := <-c
+	fmt.Printf("b1:%t,ok:%t \n",b1,ok)
+	// b1:true,ok:true
+
+
+	// 通道已关闭 通道中还有值
+	close(c)
+	b2,ok := <-c
+	fmt.Printf("b2:%t,ok:%t \n",b2,ok)
+	// b2:true,ok:true
+
+	// 通道已关闭 通道中无值
+	b3,ok := <-c
+	fmt.Printf("b3:%t,ok:%t \n",b3,ok)
+	// b3:false,ok:false
 }
