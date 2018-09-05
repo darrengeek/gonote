c1f518c1998032094afea69ef459ba0a275763ca
xmge <1206964123@qq.com>
2018-09-05 23:20:18 +0800

    'update卷yi'

--- main.go
+++ main.go
@@ -1,33 +1,23 @@
 package main
 
-import "fmt"
 
-// ok断言之通道有值
+type student struct {
+	Name string
+	Age  int
+}
+
+func pase_student() {
+	m := make(map[string]*student)
+	stus := []student{
+		{Name: "zhou", Age: 24},
+		{Name: "li", Age: 23},
+		{Name: "wang", Age: 22},
+	}
+	for _, stu := range stus {
+		m[stu.Name] = &stu
+	}
+}
+
 func main() {
-
-	c := make(chan bool,2)
-
-	<- c
-	c <- true
-	c <- true
-
-	// 通道未关闭
-	b1,ok := <-c
-	fmt.Printf("b1:%t,ok:%t \n",b1,ok)
-	// b1:true,ok:true
-
-
-	// 通道已关闭 通道中还有值
-	close(c)
-	b2,ok := <-c
-	fmt.Printf("b2:%t,ok:%t \n",b2,ok)
-	// b2:true,ok:true
-
-	// 通道已关闭 通道中无值
-	b3,ok := <-c
-	fmt.Printf("b3:%t,ok:%t \n",b3,ok)
-	// b3:false,ok:false
-
-	a := []byte{}
-	a.len()
+	pase_student()
 }
