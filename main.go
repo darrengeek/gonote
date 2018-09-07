c23c007d78643195851646f22b10ce71d3b2535d
xmge <maning@sayinfo.cn>
2018-09-07 17:22:01 +0800

    update卷一

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import "fmt"
 
+func calc(index string, a, b int) int {
+	ret := a + b
+	fmt.Println(index, a, b, ret)
+	return ret
+}
+
 func main() {
-	s := make([]int, 5)
-	s = append(s, 1, 2, 3)
-	fmt.Println(s)
+	a := 1
+	b := 2
+	defer calc("1", a, calc("10", a, b))
+	a = 0
+	defer calc("2", a, calc("20", a, b))
+	b = 1
 }
