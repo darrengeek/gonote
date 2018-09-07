a54e3dcee16d8668585d5aa73c108632156b2709
xmge <maning@sayinfo.cn>
2018-09-07 20:41:41 +0800

    update卷一

--- main.go
+++ main.go
@@ -1,34 +1,26 @@
 package main
 
-import "fmt"
-
-type People struct{}
-
-func (p *People) ShowA() {
-	fmt.Println("showA")
-	p.ShowB()
-}
-func (p *People) ShowB() {
-	fmt.Println("showB")
-}
-
-type Te struct {
-}
-
-func (t *Te) ShowA() {
-	fmt.Println("TEEEEEEEEEEEEEEE")
-}
-
-type Teacher struct {
-	People
-	Te
-}
-
-func (t *Teacher) ShowB() {
-	fmt.Println("teacher showB")
-}
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
 func main() {
-	t := Teacher{}
-	t.ShowA()
+	runtime.GOMAXPROCS(1)
+	wg := sync.WaitGroup{}
+	wg.Add(20)
+	for i := 0; i < 10; i++ {
+		go func() {
+			fmt.Println("i: ", i)
+			wg.Done()
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			fmt.Println("i: ", i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
 }
